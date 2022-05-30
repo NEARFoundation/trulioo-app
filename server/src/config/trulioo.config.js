abc003d8fa3c3d7f9006fb446e226ca353c42c69
Oleh Ivaniuk <fedencer7@gmail.com>
2022-05-30 16:10:22 +0300

    Obtaining a token for EmbedID

--- server/src/config/trulioo.config.js
+++ server/src/config/trulioo.config.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const ASYNC_CALLBACK_URL = `${process.env.EXTERNAL_SERVER_URL}/api/send-check-result`;
+export const EMBED_ID_TOKEN_URL = `https://api-gateway-admin.trulioo.com/embedids/tokens`;
+export const truliooApiKey = process.env.X_TRULIOO_API_KEY;
 
 export const truliooInstance = axios.create({
   baseURL: process.env.TRULIOO_BASE_URL,
   timeout: 10000,
-  headers: {'x-trulioo-api-key': process.env.X_TRULIOO_API_KEY}
+  headers: {'x-trulioo-api-key': truliooApiKey}
 });
