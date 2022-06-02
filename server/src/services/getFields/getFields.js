be82f4aedd69296cac33e8788f4751b6b6b6042e
Oleh Ivaniuk <fedencer7@gmail.com>
2022-06-02 18:03:53 +0300

    Bug fix: The required field for missing fields must be removed.

--- server/src/services/getFields/getFields.js
+++ server/src/services/getFields/getFields.js
@@ -10,7 +10,7 @@ export const getFields = async (req, res) => {
     const truliooInstance = req.app.get('trulioo');
     const { country } = req.query;
     const response = await truliooInstance.get(`/configuration/v1/recommendedfields/Identity%20Verification/${country}`);
-    res.send(response.data);
+    res.send(checkFieldsData(response.data));
 
   } catch (e) {
     console.log(e);
@@ -18,4 +18,22 @@ export const getFields = async (req, res) => {
       .status(500)
       .send({ error: 'The list of fields cannot be obtained. Please try again.' });
   }
+}
+
+function checkFieldsData(data) {
+  // The required field for missing fields must be removed
+  if (data && data.properties) {
+    for (const [groupId, group] of Object.entries(data.properties)) {
+      if (group && group.required) {
+        let newRequired = []
+        group.required.map(item => {
+          if (group.properties[item]) {
+            newRequired.push(item);
+          }
+        });
+        data.properties[groupId].required = newRequired;
+      }
+    }
+  }
+  return data;
 }
