f9b210a526a16af845abacb49cf3b0b9ae5b9a6c
Ryan Walsh <rcwalsh@gmail.com>
2022-08-30 11:10:00 -0400

    adding project-specific Eslint rules

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,4 +1,12 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   extends: ['near'], // See https://github.com/NEAR-Edu/eslint-config-near/blob/main/package.json
+  globals: {
+    console: true,
+    process: true,
+  },
+  rules: {
+    'canonical/sort-keys': 'off',
+    'no-console': 'off',
+  },
 };
--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   extends: ['near'], // See https://github.com/NEAR-Edu/eslint-config-near/blob/main/package.json
+  globals: {
+    console: true,
+    process: true,
+    window: true,
+  },
+  rules: {
+    'canonical/sort-keys': 'off',
+    'no-console': 'off',
+  },
 };
