7ff28b430ac63cc718efc61bdf2955cd1d046598
Ryan Walsh <rcwalsh@gmail.com>
2022-09-02 12:10:26 -0400

    reverting all changes to this file (and temporarily disabling eslint) so that the UI doesn't break after the Choose Country step

--- ui/src/store/helpers/getFields.js
+++ ui/src/store/helpers/getFields.js
@@ -1,16 +1,16 @@
-import countries from 'i18n-iso-countries';
-import enLocale from 'i18n-iso-countries/langs/en.json';
-import * as R from 'ramda';
-
+/* eslint-disable */ // TODO: re-enable eslint and fix this file.
 import { DAY_OF_BIRTH, MONTH_OF_BIRTH, YEAR_OF_BIRTH, DOB, DOB_TITLE } from './constantDateFields';
 import constantNationalIds from './constantNationalIds';
+import * as R from 'ramda';
+
+import countries from 'i18n-iso-countries';
+import enLocale from 'i18n-iso-countries/langs/en.json';
 
 countries.registerLocale(enLocale);
 
-const countryObject = countries.getNames('en', { select: 'official' });
+const countryObj = countries.getNames('en', { select: 'official' });
 
-// eslint-disable-next-line unicorn/prevent-abbreviations
-export const localeCountriesArr = Object.entries(countryObject).map(([key, value]) => {
+export const localeCountriesArr = Object.entries(countryObj).map(([key, value]) => {
   return {
     name: value,
     code: key,
@@ -20,7 +20,7 @@ export const localeCountriesArr = Object.entries(countryObject).map(([key, value
 const reservedFormDataKeys = ['countries', 'TruliooFields', 'Consents'];
 const dateFields = ['DayOfBirth', 'MonthOfBirth', 'YearOfBirth'];
 const dateFieldsMap = new Map();
-const deepCopy = (object) => JSON.parse(JSON.stringify(object));
+const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
 
 const hasDOBInMap = () => {
   const dayOfBirthInMap = dateFieldsMap.get(DAY_OF_BIRTH);
@@ -29,161 +29,164 @@ const hasDOBInMap = () => {
   return dayOfBirthInMap && monthOfBirthInMap && yearOfBirthInMap;
 };
 
-const parseFields = (object) => {
-  for (const [key] of Object.entries(object)) {
-    if (key === 0) {
-      return null;
+const parseFields = (obj) => {
+  for (const [key] of Object.entries(obj)) {
+    if (key == 0) {
+      return;
     }
-
     if (key === 'label') {
-      object.title = object[key];
+      obj.title = obj[key];
     }
-
-    parseFields(object[key]);
+    parseFields(obj[key]);
   }
-
-  return object;
+  return obj;
 };
 
-const containsDOBRequired = (required) => required && required.includes(DAY_OF_BIRTH) && required.includes(MONTH_OF_BIRTH) && required.includes(YEAR_OF_BIRTH);
-
-const updateDateRequiredArray = (object) => {
-  const { required } = object;
-
-  if (!object.properties || !required) {
-    return;
-  }
-
-  const containsDOB = containsDOBRequired(object.required);
-  if (containsDOB) {
-    object.required = required.filter((requiredField) => requiredField !== DAY_OF_BIRTH && requiredField !== MONTH_OF_BIRTH && requiredField !== YEAR_OF_BIRTH);
-    object.required.push(DOB);
-  }
-};
-
-const parseFieldDates = (object) => {
-  updateDateRequiredArray(object);
-  for (const [key] of Object.entries(object)) {
-    if (key === 0) {
-      return null;
+const parseFieldDates = (obj) => {
+  updateDateRequiredArray(obj);
+  for (const [key] of Object.entries(obj)) {
+    if (key == 0) {
+      return;
     }
-
     if (key === 'label') {
-      object.title = object[key];
+      obj.title = obj[key];
     }
-
-    if (dateFields.includes(key)) {
-      dateFieldsMap.set(key, true);
+    if (!dateFields.includes(key)) {
+      parseFieldDates(obj[key]);
     } else {
-      parseFieldDates(object[key]);
+      dateFieldsMap.set(key, true);
     }
-
-    if ((key === DAY_OF_BIRTH || key === MONTH_OF_BIRTH || key === YEAR_OF_BIRTH) && hasDOBInMap()) {
-      delete object[DAY_OF_BIRTH];
-      delete object[MONTH_OF_BIRTH];
-      delete object[YEAR_OF_BIRTH];
-      object.DOB = {
+    if (
+      (key === DAY_OF_BIRTH || key === MONTH_OF_BIRTH || key === YEAR_OF_BIRTH) &&
+      hasDOBInMap()
+    ) {
+      delete obj[DAY_OF_BIRTH];
+      delete obj[MONTH_OF_BIRTH];
+      delete obj[YEAR_OF_BIRTH];
+      obj.DOB = {
         title: DOB_TITLE,
         type: 'string',
         format: 'date',
       };
     }
   }
+  return obj;
+};
 
-  return object;
+const containsDOBRequired = (required) =>
+  required &&
+  required.includes(DAY_OF_BIRTH) &&
+  required.includes(MONTH_OF_BIRTH) &&
+  required.includes(YEAR_OF_BIRTH);
+
+const updateDateRequiredArray = (obj) => {
+  const { required } = obj;
+
+  if (!obj.properties || !required) {
+    return;
+  }
+
+  const containsDOB = containsDOBRequired(obj.required);
+  if (containsDOB) {
+    obj.required = required.filter(
+      (requiredField) =>
+        requiredField !== DAY_OF_BIRTH &&
+        requiredField !== MONTH_OF_BIRTH &&
+        requiredField !== YEAR_OF_BIRTH,
+    );
+    obj.required.push(DOB);
+  }
 };
 
 const generateConsentSchema = (consents) => {
   if (!consents || !consents.length) {
-    return null;
+    return;
   }
-
   const schema = {
     title: 'Consents',
     type: 'object',
     required: [],
     properties: {},
   };
-  for (const x of consents) {
+  consents.forEach((x) => {
     schema.required.push(x);
     schema.properties[x] = {
       title: x,
       type: 'boolean',
       default: false,
     };
-  }
-
+  });
   return schema;
 };
 
-const updateStateProvince = (object, subdivisions) => {
-  for (const key of Object.keys(object)) {
-    if (key === 'StateProvinceCode' && subdivisions.length > 0) {
-      object[key] = {
-        ...object[key],
+const updateStateProvince = (obj, subdivisions) => {
+  Object.keys(obj).forEach((k) => {
+    if (k === 'StateProvinceCode' && subdivisions.length > 0) {
+      obj[k] = {
+        ...obj[k],
         enum: subdivisions.map((x) => x.Code),
         enumNames: subdivisions.map((x) => x.Name),
       };
-    } else if (object[key] !== null && typeof object[key] === 'object') {
-      updateStateProvince(object[key], subdivisions);
+    } else if (obj[k] !== null && typeof obj[k] === 'object') {
+      updateStateProvince(obj[k], subdivisions);
     }
-  }
+  });
 };
 
 const transformNationalIdsForCountry = (nationalIds, countryCode) => {
-  if (constantNationalIds[countryCode] && nationalIds.properties) {
-    // change the description for the field to use constant national id names
-    if (nationalIds.properties.Number && nationalIds.properties.Number.description) {
-      nationalIds.properties.Number.description = constantNationalIds[countryCode].map((nationalId) => `(${nationalId.name})`).join(', ');
-    }
-
-    // change the Type select to use national id names instead of GG national id types
-    if (nationalIds.properties.Type && nationalIds.properties.Type.enum) {
-      nationalIds.properties.Type.enum = constantNationalIds[countryCode].map((nationalId) => nationalId.name);
-    }
-
-    // remove Type select if only one option in select
-    if (constantNationalIds[countryCode].length < 2 && nationalIds.properties.Type) {
-      delete nationalIds.properties.Type;
-      if (nationalIds.required && nationalIds.required.length === 2) {
-        nationalIds.required = [nationalIds.required[0]];
+  if (constantNationalIds[countryCode]) {
+    if (nationalIds.properties) {
+      // change the description for the field to use constant national id names
+      if (nationalIds.properties.Number && nationalIds.properties.Number.description) {
+        nationalIds.properties.Number.description = constantNationalIds[countryCode]
+          .map((nationalId) => `(${nationalId.name})`)
+          .join(', ');
+      }
+      // change the Type select to use national id names instead of GG national id types
+      if (nationalIds.properties.Type && nationalIds.properties.Type.enum) {
+        nationalIds.properties.Type.enum = constantNationalIds[countryCode].map(
+          (nationalId) => nationalId.name,
+        );
+      }
+      // remove Type select if only one option in select
+      if (constantNationalIds[countryCode].length < 2 && nationalIds.properties.Type) {
+        delete nationalIds.properties.Type;
+        if (nationalIds.required && nationalIds.required.length === 2) {
+          nationalIds.required = [nationalIds.required[0]];
+        }
       }
     }
   }
 };
 
-const parseSubmitTruliooDateFields = (object) => {
-  for (const key of Object.keys(object)) {
+const parseSubmitTruliooDateFields = (obj) => {
+  Object.keys(obj).forEach((key) => {
     if (key === DOB) {
-      const splitDate = object[key].split('-');
+      const splitDate = obj[key].split('-');
       const [year, month, day] = splitDate;
-      object[DAY_OF_BIRTH] = day;
-      object[MONTH_OF_BIRTH] = month;
-      object[YEAR_OF_BIRTH] = year;
-      delete object[DOB];
+      obj[DAY_OF_BIRTH] = day;
+      obj[MONTH_OF_BIRTH] = month;
+      obj[YEAR_OF_BIRTH] = year;
+      delete obj[DOB];
     }
-
-    if (typeof object[key] === 'object') {
-      parseSubmitTruliooDateFields(object[key]);
+    if (typeof obj[key] === 'object') {
+      parseSubmitTruliooDateFields(obj[key]);
     }
-  }
-
-  return object;
+  });
+  return obj;
 };
 
 const parseTruliooFields = (formData) => {
   const truliooFields = {};
-  for (const key of Object.keys(formData)) {
+  Object.keys(formData).forEach((key) => {
     if (reservedFormDataKeys.includes(key)) {
       truliooFields[key] = formData[key];
     }
-  }
-
+  });
   if (hasDOBInMap()) {
     const parsedFieldsWithDates = parseSubmitTruliooDateFields(truliooFields);
     return parsedFieldsWithDates;
   }
-
   return truliooFields;
 };
 
@@ -193,52 +196,51 @@ const getNationalIdsForGG = (nationalIds, countryCode) => {
       return [
         {
           ...nationalIds,
-          Type: constantNationalIds[countryCode].find((natIds) => natIds.name === nationalIds.Type).type,
+          Type: constantNationalIds[countryCode].find((natIds) => natIds.name === nationalIds.Type)
+            .type,
         },
       ];
     }
-
     if (nationalIds.Number) {
       return [{ ...nationalIds, Type: constantNationalIds[countryCode][0].type }];
     }
-
-    return null;
+    return;
   }
-
   return [nationalIds];
 };
 
 const getWhiteListedFieldsOnly = (fields, whiteListedTruliooFields, whiteListedComputedFields) => {
-  for (const key of Object.keys(whiteListedTruliooFields)) {
+  Object.keys(whiteListedTruliooFields).forEach((key) => {
     const keyExists = Object.prototype.hasOwnProperty.call(fields, key);
     // key is not contained in fields
     if (!keyExists) {
-      continue;
+      return;
     }
-
     const hasDefinedChildren = Object.keys(whiteListedTruliooFields[key]).length > 0;
     if (hasDefinedChildren) {
       whiteListedComputedFields[key] = {};
       if (fields.title) {
         whiteListedComputedFields.title = fields.title;
       }
-
       if (fields.type) {
         whiteListedComputedFields.type = fields.type;
       }
-
       if (fields.required) {
         const childProperties = Object.keys(whiteListedTruliooFields.properties);
-        const whiteListedRequiredFields = fields.required.filter((requiredField) => childProperties.includes(requiredField));
+        const whiteListedRequiredFields = fields.required.filter((requiredField) =>
+          childProperties.includes(requiredField),
+        );
         whiteListedComputedFields.required = whiteListedRequiredFields;
       }
-
-      getWhiteListedFieldsOnly(fields[key], whiteListedTruliooFields[key], whiteListedComputedFields[key]);
+      getWhiteListedFieldsOnly(
+        fields[key],
+        whiteListedTruliooFields[key],
+        whiteListedComputedFields[key],
+      );
     } else {
       whiteListedComputedFields[key] = fields[key];
     }
-  }
-
+  });
   return whiteListedComputedFields;
 };
 
@@ -246,26 +248,22 @@ const parseFormData = (form) => {
   if (form === undefined || form.TruliooFields === undefined) {
     return form;
   }
-
   if (form.TruliooFields.Document) {
-    const documentFront = form.TruliooFields.Document.DocumentFrontImage;
-    form.TruliooFields.Document.DocumentFrontImage = documentFront.slice(documentFront.indexOf(',') + 1);
-    const documentBack = form.TruliooFields.Document.DocumentBackImage;
-    if (documentBack) {
-      form.TruliooFields.Document.DocumentBackImage = documentBack.slice(documentBack.indexOf(',') + 1);
+    const docFront = form.TruliooFields.Document.DocumentFrontImage;
+    form.TruliooFields.Document.DocumentFrontImage = docFront.substr(docFront.indexOf(',') + 1);
+    const docBack = form.TruliooFields.Document.DocumentBackImage;
+    if (docBack) {
+      form.TruliooFields.Document.DocumentBackImage = docBack.substr(docBack.indexOf(',') + 1);
     }
-
     const livePhoto = form.TruliooFields.Document.LivePhoto;
     if (livePhoto) {
-      form.TruliooFields.Document.LivePhoto = livePhoto.slice(livePhoto.indexOf(',') + 1);
+      form.TruliooFields.Document.LivePhoto = livePhoto.substr(livePhoto.indexOf(',') + 1);
     }
   }
-
   if (form.TruliooFields.NationalIds) {
     const nationalIds = form.TruliooFields.NationalIds;
     form.TruliooFields.NationalIds = getNationalIdsForGG(nationalIds, form.countries);
   }
-
   return form;
 };
 
@@ -275,18 +273,20 @@ const getCountryCode = (form) => {
       return value;
     }
   }
-
-  return null;
 };
 
 const validateAdditionalFields = (additionalFields) => {
   if (!additionalFields || !additionalFields.properties) {
     return;
   }
-
-  const containsReservedKeys = R.intersection(Object.keys(additionalFields.properties), reservedFormDataKeys);
+  const containsReservedKeys = R.intersection(
+    Object.keys(additionalFields.properties),
+    reservedFormDataKeys,
+  );
   if (containsReservedKeys.length > 0) {
-    throw new Error(`${containsReservedKeys.toString()} is a reserved field key. Please use different naming for your additional fields.`);
+    throw Error(
+      `${containsReservedKeys.toString()} is a reserved field key. Please use different naming for your additional fields.`,
+    );
   }
 };
 
@@ -295,26 +295,24 @@ const parseConsents = (consents) => {
   if (consents === undefined) {
     return result;
   }
-
-  for (const x of Object.keys(consents)) {
+  Object.keys(consents).forEach((x) => {
     /* istanbul ignore else */
     if (consents[x]) {
       result.push(x);
     }
-  }
-
+  });
   return result;
 };
 
 const getSubmitBody = (form) => {
   const countryCode = getCountryCode(form);
-  const parsedForm = parseFormData(form);
+  form = parseFormData(form);
 
   return {
     session_id: null,
     country: countryCode,
-    fields: parsedForm.TruliooFields,
-    consents: parseConsents(parsedForm.Consents),
+    fields: form.TruliooFields,
+    consents: parseConsents(form.Consents),
   };
 };
 
