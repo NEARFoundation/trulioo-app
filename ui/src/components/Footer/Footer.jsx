817b18e56d1b315b1d7ffba628b6ec67342f5bfa
Sandoche <sandoche@gmail.com>
2022-09-22 14:51:27 +0100

    Adding react router link

--- ui/src/components/Footer/Footer.jsx
+++ ui/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { Box, Link } from '@mui/material';
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
 import { useStyles } from './Footer.styles';
 
@@ -10,7 +11,7 @@ export default function Footer() {
     <footer>
       <Box py={6} display="flex" flexWrap="wrap" alignItems="center" className={classes.footerContainer}>
         <Box component="nav" className={classes.footerNav}>
-          <Link href="/privacy-policy" variant="body1" color="textPrimary" className={classes.footerLink}>
+          <Link component={RouterLink} to="/privacy-policy" variant="body1" color="textPrimary" className={classes.footerLink}>
             Privacy Policy
           </Link>
         </Box>
